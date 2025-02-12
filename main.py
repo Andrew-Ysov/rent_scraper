efb68b8adb5482650ceca203ec90b7f01de2b742
Усов Андрей <destracthioner@gmail.com>
2025-02-12 11:45:01 +0300

    улучшено удобство импортирования и тестирования

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from ligakvartir_lug_parser import site_parse
 from vk_parse import post_parse
 from data_and_file_writer import site_link
 
-site_parse(site_link)
-post_parse()
+if __name__ == '__main__':
+    site_parse(site_link)
+    post_parse()
