8f34b6069ed1db0c7902071c791336f28ec72809
Усов Андрей <destracthioner@gmail.com>
2025-02-19 14:46:22 +0300

    добавлено блок name==main

--- data_and_file_writer.py
+++ data_and_file_writer.py
@@ -8,8 +8,12 @@ num_of_posts = 20
 site_link = 'https://www.ligakvartir.ru/lugansk/snyat-nedvizhimost'
 file_name = 'offers.csv'
 
+
 def file_writer(data):
         with open(file_name, 'a', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
                 writer.writerow(data)
 
+
+if __name__ == '__main__':
+        pass
